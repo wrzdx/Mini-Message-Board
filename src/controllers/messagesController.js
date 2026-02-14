4bcaebd6460c61df9afc6d52e800ccefe931c19a
Evgenii <7zhhovalyg@mail.ru>
2026-02-15 02:09:35 +0300

    Display error msgs

--- src/controllers/messagesController.js
+++ src/controllers/messagesController.js
@@ -1,4 +1,5 @@
 import db from "../db/queries.js"
+import { body, validationResult, matchedData } from "express-validator"
 
 function formatDate(date) {
   const d = new Date(date)
@@ -12,16 +13,33 @@ function formatDate(date) {
   })
 }
 
+const validateMessage = [
+  body("messageText").trim().notEmpty().withMessage("Message text cannot be empty"),
+  body("messageUser").trim().notEmpty().withMessage("Message user cannot be empty"),
+]
+
 const getMessages = async (req, res) => {
   const messages = await db.getMessages()
   res.render("index", { messages, formatDate })
 }
 
-const postMessage = async (req, res) => {
-  const messageText = req.body.messageText || "Empty Message"
-  const messageUser = req.body.messageUser || "Guest"
-  await db.createMessage(messageUser, messageText)
-  res.redirect("/")
-}
+const postMessage = [
+  validateMessage,
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      const messages = await db.getMessages()
+      console.log(errors.array())
+      return res.render("index", {
+        messages,
+        formatDate,
+        errors: errors.array(),
+      })
+    }
+    const { messageText, messageUser } = matchedData(req)
+    await db.createMessage(messageUser, messageText)
+    res.redirect("/")
+  },
+]
 
 export default { getMessages, postMessage }
